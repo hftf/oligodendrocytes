6d1c66c944a97b0ba87d9d0a3c3d2cb5d9de7017
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-02-04 01:53:19 -0500

    Handling unwanted whitespace in .edges

--- transformers/this-qbml-to-prev-edges.sh
+++ transformers/this-qbml-to-prev-edges.sh
@@ -6,22 +6,25 @@ THIS=${THIS##*/}
 PREV=$(awk "/$THIS/ { print line; } { line = \$0 } " order.txt)
 PREV_QBML=$DIR/$PREV.qbml
 THIS_EDGES=$1
-echo $DIR
-echo $THIS
-echo $PREV_QBML
-echo $PREV
-echo $THIS_EDGES
+# echo $DIR
+# echo $THIS
+# echo $PREV_QBML
+# echo $PREV
+# echo $THIS_EDGES
 
 # TODO fix whitepsace
 XSLT="xsltproc transformers/qbml-to-latex.xsl -"
+SED='sed s/\\answer//g'
+TR="tr -d '\n'"
+
 if [ -z "$PREV" ]; then
     > $THIS_EDGES
 else
     printf '\\renewcommand\lastpacketname{'                                       > $THIS_EDGES
-    xpath "$PREV_QBML" "translate(string(//packet/@name), '\n', '')" 2>/dev/null >> $THIS_EDGES
+    xpath "$PREV_QBML" "string(//packet/@name)" 2>/dev/null | $TR                >> $THIS_EDGES
     printf '}\n\\renewcommand\lastpacketone{'                                    >> $THIS_EDGES
-    xpath "$PREV_QBML" "//tossup[1]/answer"  2>/dev/null | $XSLT                 >> $THIS_EDGES
+    xpath "$PREV_QBML" "//tossup[1]/answer"  2>/dev/null | $XSLT | $SED | $TR    >> $THIS_EDGES
     printf '}\n\\renewcommand\lastpackettwenty{'                                 >> $THIS_EDGES
-    xpath "$PREV_QBML" "//tossup[20]/answer" 2>/dev/null | $XSLT                 >> $THIS_EDGES
+    xpath "$PREV_QBML" "//tossup[20]/answer" 2>/dev/null | $XSLT | $SED | $TR    >> $THIS_EDGES
     printf '}\n'                                                                 >> $THIS_EDGES
 fi
