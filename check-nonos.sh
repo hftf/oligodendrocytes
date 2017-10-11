664bd5f4e710cc5c947defb28921e25983866919
Ophir Lifshitz <hangfromthefloor@gmail.com>
2017-10-11 05:17:53 -0400

    Minor rearrangements and additions to check-nonos.sh

--- check-nonos.sh
+++ check-nonos.sh
@@ -5,32 +5,44 @@ PREFIX="$@"
 NONOS="or ten point| each, [^BPn]| each\.|[^0-9]--|\.\.| \.|…\.?\S| ,|”’|‘“|“ |‘ | ’| ”|’\.|[^!]’,|”\.|[^!]”,|,[^ ’”*0-9]|’\s?”| |  |'|\"| \[|\.\*\* \(|ANSWER:\S|ANSWER[^:]|ANWER|\\[^*\n$]| "
 grep --color=always -EHn "$NONOS" $PREFIX*.md
 
-echo
-echo "(\*)"
-grep --color=always -EiHno '.{,5}\(\\\*\)\S.{,5}' $PREFIX*.md || :
+#echo
+#echo "(\*)"
+#grep --color=always -EiHno '.{,5}\(\\\*\)\S.{,5}' $PREFIX*.md || :
 
 echo
 echo "space after newline"
 grep --color=always -EiHnc "^(​|\s)" $PREFIX*.md || :
 
 echo
+echo "****"
+grep --color=always -EiHn "\*\*\*\*" $PREFIX*.md || :
+
+#echo
+#echo "* *"
+#not useful for PGs written with <span><b>
+####grep --color=always -EiHn "..\* \*.." $PREFIX*.md | grep -v ANSWER || :
+
+
+echo
+echo '"answers required." "Description acceptable." and titles with ending punctuation'
+echo "[,.?!]*"
+grep --color=always -PiHn "(?<!\.\w)[.,?!]\*(?!\*)" $PREFIX*.md || :
+
+#echo
+#echo " \["
+#not useful for PGs written with []
+####grep --color=always -PiHn " \[" $PREFIX*.md | grep -v ANSWER || :
+
+#echo
+#echo " </"
+#grep --color=always -EiHn " </" $PREFIX*.o.html || :
+
+
+echo
 echo -e "\t  tossups bonuses   parts"
 for i in $PREFIX*.qbml; do
 echo -e "$i`grep -o '<tossup ' $i | wc -l``grep -o '<bonus ' $i | wc -l``grep -o '<part ' $i | wc -l`"
 done
-
-echo
-echo "****"
-grep --color=always -EiHn "\*\*\*\*" $PREFIX*.md || :
-
-echo
-echo "* *"
-grep --color=always -EiHn "..\* \*.." $PREFIX*.md | grep -v ANSWER || :
-
-
-echo
-echo "[,.?!]*"
-grep --color=always -PiHn "(?<!\.\w)[.,?!]\*(?!\*)" $PREFIX*.md || :
 
 echo
 echo "\\answer{}"
