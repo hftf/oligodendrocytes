1ff051f5024086bdc0648c3539108aa543081a0a
Ophir Lifshitz <hangfromthefloor@gmail.com>
2017-10-11 17:34:51 -0400

    Don't index non-content words in number.js

--- transformers/bundle-assets/number.js
+++ transformers/bundle-assets/number.js
@@ -1,13 +1,23 @@
 var FAR = findAndReplaceDOMText;
+// http://velocityjs.org/blast/
 
 function getPs() {
 	var x = document.body.getElementsByClassName('tu');
 	var j = 0;
 
+	// using unnamed group to keep ^ and $ outside of | scope
+	var no = /^(?:\(\*\)|[\/,\.“”]+)$/;
+
+	// TODO fix final punctuation kerning (foo</m>.)
+
 	function replace(portion) {
+		var tn = document.createTextNode(portion.text);
+		if (no.test(portion.text))
+			return tn;
+
 		var e = document.createElement('m');
 		e.dataset.v = j ++;
-		e.appendChild(document.createTextNode(portion.text));
+		e.appendChild(tn);
 		return e;
 	}
 
@@ -16,7 +26,7 @@ function getPs() {
 		j = 0;
 
 		FAR(p, {
-			find: /[^ (*)]+/g,
+			find: /[^  ]+/g,
 			replace: replace
 		});
 	}
