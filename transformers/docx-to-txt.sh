aab9ca62e13d3c7b55777ec29382c89fd465e937
Ophir Lifshitz <hangfromthefloor@gmail.com>
2017-10-12 07:04:09 -0400

    Add tregex queries and sample tree

--- transformers/docx-to-txt.sh
+++ transformers/docx-to-txt.sh
@@ -7,3 +7,9 @@ textutil -convert txt $FILENAME -stdout \
 	| perl -CSD -pe 's/\x{2028}|\x0c/\n/g' \
 	| sed -E 's/^ANSWER: .*|^[0-9]+\. |For 10 points each:|\[10] //g' \
 	| perl -pe 's/\s[\(\[](?!(?:this|these|his|here|one|do|\d) )[^\)\]]+[\)\]]//g'
+
+# tregex.sh -f -n -w -s -i experiments/nlp-parsing/tregex-queries/1.txt -e parsed experiments/nlp-parsing/ > trees.txt
+# perl -pe 's/(?<=\()(SBAR|S)\b/\1 [\1/g; s/\(\S+ |\)//g; s!^#.*/([^.]+)[^/]+$!\1\t!g; s/(\d+): (\[S )?//g' trees.txt
+
+# TODO 1 ignore called, named
+# TODO 2 ignore if in quotes/italics
