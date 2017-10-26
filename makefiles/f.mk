abf116f96430dedbca7435f63abf3c8eeb8a518c
Ophir Lifshitz <hangfromthefloor@gmail.com>
2017-10-26 04:30:36 -0400

    Template: Split transformers/bottom.html

--- makefiles/f.mk
+++ makefiles/f.mk
@@ -1,6 +1,6 @@
 .PRECIOUS: %.f.html %.r.html
 
-%.f.html: %.o.html transformers/o-html-to-f-html.sh transformers/top-1.html transformers/top-2.html
+%.f.html: %.o.html transformers/o-html-to-f-html.sh transformers/top-1.html transformers/top-2.html transformers/bottom.html
 	$(word 2,$^) $< > $@
 
 %.r.html: %.f.html transformers/f-html-to-r-html.py transformers/htmlparser.py
--- transformers/o-html-to-f-html.sh
+++ transformers/o-html-to-f-html.sh
@@ -11,4 +11,5 @@ cat transformers/top-2.html | sed "s@FILENAMER@$FILENAMER@g"
 	gsed -E -e '0,/^<body>$$/d' \
 	        -e 's/<p class="p1">ANSWER/<p class="p1 answer">ANSWER/g' \
 	        -e '1,/>Bonuses/I s/<p class="p1">([0-9]+\. )/<p class="p1 tu">\1/g' \
-	        -e 's/<\/body>/<script src="clipboard-polyfill.js"><\/script><script src="findAndReplaceDOMText.js"><\/script><script src="number.js"><\/script><\/body>/'
+	        -e '/^<\/body>$$/,$d'
+cat transformers/bottom.html
