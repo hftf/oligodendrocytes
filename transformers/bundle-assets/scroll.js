419bd9adda69ecdb1eeb0ece198f2d8ece041028
Ophir Lifshitz <hangfromthefloor@gmail.com>
2017-11-15 16:31:13 -0500

    Scroll button: Toggle label

--- transformers/bundle-assets/scroll.js
+++ transformers/bundle-assets/scroll.js
@@ -5,12 +5,33 @@ function swap() {
 	tmp = window.pageYOffset;
 	window.scroll(0, scr);
 	scr = tmp;
+	label();
 }
-function setHandler2() {
-	toggle.onclick = swap;
+function label() {
+	var newHTML;
+	if (window.pageYOffset > scr)
+		newHTML = '↑ Up to next tossup';
+	else
+		newHTML = '↓ Down to next bonus';
+
+	if (toggle.innerHTML != newHTML)
+		toggle.innerHTML = newHTML;
 }
 
 function loadScroll() {
 	initialScrolls();
-	setHandler2();
+
+	// button: set click handler, initial label
+	toggle.onclick = swap;
+	label();
+
+	// scroll handler
+	/*var didScroll = false;
+	window.onscroll = function() { didScroll = true; };
+	setInterval(function() {
+		if (didScroll) {
+			didScroll = false;
+			label();
+		}
+	}, 400);*/
 }
