f975ae1e5ffd89855ead4c4451f09ab75e876415
Ophir Lifshitz <hangfromthefloor@gmail.com>
2017-11-16 16:13:14 -0500

    Makefile: Add w.html target

--- makefiles/f.mk
+++ makefiles/f.mk
@@ -5,3 +5,10 @@
 
 %.r.html: %.f.html transformers/f-html-to-r-html.py transformers/htmlparser.py
 	$(word 2,$^) $< > $@
+
+HTMLS:=$(call FORMATS,w.html)
+htmls: $(HTMLS)
+
+# TODO depends on node
+%.w.html: %.r.html transformers/f-html-to-w-html.js
+	$(word 2,$^) $< > $@
