c799e34174088ae50a30c61afc8d3407ff1eb601
Ophir Lifshitz <hangfromthefloor@gmail.com>
2019-10-15 03:02:46 -0400

    Template: More robust o-html-to-f-html.sh

--- transformers/o-html-to-f-html.sh
+++ transformers/o-html-to-f-html.sh
@@ -6,10 +6,12 @@ cat transformers/top-1.html
 	gsed -E 's/font: ([0-9]+)\.0px/font: \10%/g'
 cat transformers/top-2.html
 <$FILENAME \
+	perl -p -e 's/<br>\n/<\/p>\n<p>/;' \
+	     -p -e 's/<p><\/p>\n//;' |
 	gsed -E -e '0,/^<body>$$/d' \
 	        -e 's/<p class="p1">ANSWER/<p class="p1 answer">ANSWER/g' \
-	        -e '1,/>Bonuses/I s/<p class="p1">([0-9]+\. )/<p class="p1 tu">\1/g' \
-	        -e '/>Bonuses/I,$ s/<p class="p1">([0-9]+\. )/<p class="p1 bonus">\1/g' \
-	        -e 's/>Bonuses/ id="bonuses"&/' \
+	        -e '1,/>Bonuses/I s/<p class="p1">([A-Za-z0-9]+\. )/<p class="p1 tu">\1/g' \
+	        -e '/>Bonuses/I,$ s/<p class="p1">([A-Za-z0-9]+\. )/<p class="p1 bonus">\1/g' \
+	        -e 's/>Bonuses/ id="bonuses"&/I' \
 	        -e '/^<\/body>$$/,$d'
 cat transformers/bottom.html
