7cc14d8334f4a4055df0e4942b5258b92c18fddc
Ophir Lifshitz <hangfromthefloor@gmail.com>
2019-10-21 02:09:11 -0400

    Experiments: Split unparsed and parsed in nlp make

--- makefiles/nlp-parser.mk
+++ makefiles/nlp-parser.mk
@@ -3,5 +3,8 @@
 %.txt: %.docx transformers/docx-to-txt.sh
 	$(word 2,$^) "$<" > "$@"
 
-%.txt.parsed: %.txt
+%.txt.unparsed: %.txt transformers/docx-to-txt-unparsed.sh
+	$(word 2,$^) "$<" > "$@"
+
+%.txt.parsed: %.txt.unparsed
 	lexparser.sh "$<" > "$@"
--- transformers/docx-to-txt.sh
+++ transformers/docx-to-txt.sh
@@ -1,17 +1,3 @@
 FILENAME="$1"
 
-# TODO delete Bonuses line? delete tiebreaker instructions?
-# TODO remove PGs and (*) in less naive way
 textutil -convert txt $FILENAME -stdout
-# textutil -convert txt $FILENAME -stdout \
-# 	| sed '1,/Tossups/d' \
-# 	| perl -CSD -pe 's/\x{2028}|\x0c/\n/g' \
-# 	| sed -E 's/^ANSWER: .*|^[0-9]+\. |^<.*>|, for 10 points,| For 10 points each:|\[10] //g' \
-# 	| gsed -E 's/For 10 points, (\W+)(.)/\1\u\2/g' \
-# 	| perl -pe 's/\s[\(\[](?!(?:this|these|that|those|his|here|one|do|\d) )[^\)\]]+[\)\]]//g'
-
-# tregex.sh -f -n -w -s -i experiments/nlp-parsing/tregex-queries/1.txt -e parsed experiments/nlp-parsing/ > trees.txt
-# perl -pe 's/(?<=\()(SBAR|S)\b/\1 [\1/g; s/\(\S+ |\)//g; s!^#.*/([^.]+)[^/]+$!\1\t!g; s/(\d+): (\[S )?//g' trees.txt
-
-# TODO 1 ignore called, named
-# TODO 2 ignore if in quotes/italics
