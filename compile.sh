6a206d0fb03ba75544014c0ec738452324b5eeae
Ophir Lifshitz <hangfromthefloor@gmail.com>
2020-03-26 23:34:34 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,28 +1,15 @@
-tournament="terrapin3"
-base_tournament="terrapin2"
+tournament="mytournament"
+base_tournament="oldtournament"
 
 # create tournaments/$tournament folder structure
 
 echo "$tournament" > tournaments/current.txt
-mkdir tournaments/$tournament
-mkdir tournaments/$tournament/packets
+mkdir -p tournaments/$tournament/packets
 
-# copy settings and packet order
+# copy settings
 
-cp tournaments/$base_tournament/settings.pxml tournaments/$tournament/
-cp tournaments/$base_tournament/order.txt tournaments/$tournament/
-
-# create $tournament-packets/ folder structure
-
-mkdir $tournament-packets/
-cd $tournament-packets/
-mkdir docx docx-password html pdf pdf-password
-cd -
-touch $tournament-packets/html/\!please-remember-to-fully-extract-the-zip-archive
-
-# copy assets for html
-
-cp $base_tournament-packets/html/!(*.f.html) $tournament-packets/html
+cp -n tournaments/$base_tournament/settings.pxml tournaments/$tournament/
+# manually edit settings.pxml
 
 # checks
 # TODO script to test if checks pass
@@ -37,22 +24,38 @@ cp $base_tournament-packets/html/!(*.f.html) $tournament-packets/html
 make reset
 make -j5 formats EXT=f.html
 
-# copy the docx and f.html packets
+# create web packets bundle folder structure
 
-cp tournaments/$tournament/packets/*.f.html $tournament-packets/html/
-cp tournaments/$tournament/packets/*.docx $tournament-packets/docx/
+bundle="2019-tournament"
+edition="2019-10-01"
+mkdir -p bundles/$bundle/$edition/html/
 
-# create the pdf and password protected packets
+# manually create folder structure on host
 
-# TODO don't require cd'ing into here
+host="myhost.buzz"
+hostpath="path/to"
+# ssh $host "cd $hostpath && cp -R packets packets-$prev_edition"
+
+# copy the web packets bundle
+
+cp tournaments/$tournament/packets/{*.w.html,*.js,!(fonts).css,*.php,*.png,*.csv} bundles/$bundle/$edition/html/
+cp tournaments/$tournament/packets/{*.o.html,*.md,*.md.nowrap}                    bundles/$bundle/$edition/html/
+scp bundles/$bundle/$edition/html/{*.w.html,*.js,!(fonts).css,*.php,*.png,*.csv}  $host:$hostpath/tournaments/$tournament/packets/html/
+
+# download pdf/docx packets by exporting directly from google drive
+
+drive pull -export pdf  -exports-dir tournaments/$tournament/packets/docs/pdf/ -explicitly-export -same-exports-dir "Quizbowl/$PACKETS_DIR/Packets"
+drive pull -export docx -exports-dir tournaments/$tournament/packets/docs/pdf/ -explicitly-export -same-exports-dir "Quizbowl/$PACKETS_DIR/Packets"
+
+# create the pdf and password protected pdf packets (zips)
+
+cp -n password.sh tournaments/$tournament/packets/docs/pdf/
+# manually edit password.sh
 # TODO don't hardcode stuff in password.sh
-cd $tournament-packets/docx/
+# TODO don't require cd'ing into here
+cd tournaments/$tournament/packets/docs/pdf/
 ./password.sh
-cd -
-
-# cp password.sh $tournament-packets/docx/password.sh
-# cp number.js $tournament-packets/html/number.js
-
-# create zip
-rm $tournament-packets.zip
-zip $tournament-packets.zip -r $tournament-packets/
+zip $tournament-password-pdfs-$edition.zip *.password.pdf
+zip $tournament-pdfs-$edition.zip          !(*password).pdf
+zip $tournament-docxs-$edition.zip         !(*password).docx
+scp $tournament-password-pdfs-$edition.zip $host:$hostpath/tournaments/$tournament/packets/html/password-pdfs.zip
