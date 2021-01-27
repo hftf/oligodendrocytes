a4d12e6d19c7bfd645236609b0a4b78c0ee5fdaa
Ophir Lifshitz <hangfromthefloor@gmail.com>
2021-01-26 20:38:24 -0500

    Relax patterns (bonuses delimiter, bonus parts)

--- transformers/o-html-to-f-html.sh
+++ transformers/o-html-to-f-html.sh
@@ -5,13 +5,14 @@ cat transformers/top-1.html
 	sed -n '/<style/,/<\/style/p' | \
 	gsed -E 's/font: ([0-9]+)\.0px/font: \10%/g'
 cat transformers/top-2.html
+# search history for FILENAMER
 <$FILENAME \
 	perl -p -e 's/<br>\n/<\/p>\n<p>/;' \
 	     -p -e 's/<p><\/p>\n//;' |
 	gsed -E -e '0,/^<body>$$/d' \
 	        -e 's/<p class="p1">ANSWER/<p class="p1 answer">ANSWER/g' \
-	        -e '1,/>Bonuses/I s/<p class="p1">([A-Za-z0-9]+\. )/<p class="p1 tu">\1/g' \
-	        -e '/>Bonuses/I,$ s/<p class="p1">([A-Za-z0-9]+\. )/<p class="p1 bonus">\1/g' \
+	        -e '1,/>(.* )?Bonuses/I s/<p class="p1">([A-Za-z0-9]+\. )/<p class="p1 tu">\1/g' \
+	        -e '/>(.* )?Bonuses/I,$ s/<p class="p1">([A-Za-z0-9]+\. )/<p class="p1 bonus">\1/g' \
 	        -e 's/>Bonuses/ id="bonuses"&/I' \
 	        -e '/^<\/body>$$/,$d'
 cat transformers/bottom.html
--- transformers/txt-to-txt-clean.sh
+++ transformers/txt-to-txt-clean.sh
@@ -5,7 +5,7 @@ FILENAME="$1"
 < "$FILENAME" \
 	  sed '1,/Tossups/d' \
 	| perl -CSD -pe 's/\x{2028}|\x0c/\n/g' \
-	| sed -E 's/^ANSWER: .*|^[0-9]+\. |^<.*>|, for 10 points,|, for 10 points each| For 10 points each:|\[10] //g' \
+	| sed -E 's/^ANSWER: .*|^[0-9]+\. |^<.*>|, for 10 points,|, for 10 points each| For 10 points each:|\[10[emh]?] //g' \
 	| gsed -E 's/For 10 points, (\W*)(.)/\1\u\2/g' \
 	| perl -pe 's/ \((?:\*|“[^)]+)\)//g' \
 	| perl -pe 's/ \[(?:read slowly|pause|emphasize)\]//g'
