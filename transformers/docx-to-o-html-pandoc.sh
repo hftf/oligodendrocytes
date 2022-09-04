e3221c26141f611640d0a602b9bc10300b5250ae
Ophir Lifshitz <hangfromthefloor@gmail.com>
2022-09-04 05:54:43 -0400

    Basic template for docx-to-o-html-pandoc

--- transformers/docx-to-o-html-pandoc.sh
+++ transformers/docx-to-o-html-pandoc.sh
@@ -1,6 +1,12 @@
 FILENAME="$1"
+TEMPLATE="$2"
+if [ -z "$TEMPLATE" ]; then
+	TEMPLATE="transformers/template.o.html"
+fi
 
 # TODO change space to \s
-pandoc "$FILENAME" -f docx+empty_paragraphs -t html+empty_paragraphs -s \
+pandoc "$FILENAME" -f docx+empty_paragraphs -t html+empty_paragraphs \
+	--wrap=none \
+	-s --template "$TEMPLATE" \
 	| sed -E "s/ ((<[\/][^>]*>)+)/\1 /g" \
 	| sed -E "s/((<[^/][^>]*>)+) / \1/g"
