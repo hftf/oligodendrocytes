9a86d16e1ccad0fdedcb281a4b04d2c53711c2fb
Ophir Lifshitz <hangfromthefloor@gmail.com>
2024-02-20 17:12:14 -0500

    Improve check revealed words

--- transformers/check-revealed-answer.py
+++ transformers/check-revealed-answer.py
@@ -8,10 +8,10 @@ import lemminflect
 
 # START_OF_QUESTION_OR_PART = r'^\s*(?:\d+\.)?\s*'
 QUESTION_regex = r'<p class="'
-ANSWER_regex = r'<p class="answer">.*?</p>'
+ANSWER_regex = r'<p class="answer">.*?</p>\n?'
 TOSSUPS_regex = r'Tossups'
 # note: will not catch e.g. "A major" because "A" is a stopword
-STOPWORDS = re.compile(r'(?i)^(a|the|an|in|on|to|for|de|of|and|is|was|were|are|or|but|by|not|i|from|that|this|with)$')
+STOPWORDS = re.compile(r'(?i)^(|a|the|an|in|on|to|for|de|of|and|is|was|were|are|or|but|by|not|i|from|that|this|with)$')
 # TODO does not catch inflected forms because I'm using \b
 # e.g. echinodermata echinoderms
 
@@ -21,8 +21,9 @@ def get_inflected_forms_regex(word):
 	# lemmatize the word:
 	for upos in ['NOUN', 'VERB', 'ADV', 'ADJ']:
 		lemma = lemminflect.getLemma(word, upos)
+		yield lemma[0]
 		inflections = lemminflect.getAllInflections(lemma[0])
-		print (lemma, inflections)
+		# print (lemma, inflections)
 		for values in inflections.values():
 			for v in values:
 				yield re.escape(v)
@@ -31,6 +32,8 @@ def check_revealed_answer(contents):
 	# find start and end positions of all matches for ANSWER_regex
 	split = split_into_text_and_answers(contents)
 
+	current_question_number = 0
+	current_question_part = 0
 	current_question_text = ''
 	current_required_words = set()
 	for span in split:
@@ -38,11 +41,19 @@ def check_revealed_answer(contents):
 		if span_begins_new_question:
 			current_question_text = span['text'][span_begins_new_question.start():]
 			current_required_words = set(span['required_words'])
+			current_question_number += 1
+			current_question_part = 0
+			sys.stderr.write(f'\n\033[7m Question {current_question_number} \033[0m\n')
+
 		else:
 			current_question_text += span['text']
 			current_required_words = set(span['required_words'])
+		current_question_part += 1
+		sys.stderr.write(f'\033[7m Answer   {current_question_part} \033[0m  ')
 
 		required_words_regex = re.compile(required_words_as_regex(sorted(current_required_words)))
+		sys.stderr.write(f'\033[37m{required_words_regex.pattern}\033[0m\n')
+
 		match = re.search(required_words_regex, current_question_text)
 		if match:
 			highlighted_text = re.sub(required_words_regex, '\033[102m\g<0>\033[0m', current_question_text)
@@ -53,7 +64,6 @@ def check_revealed_answer(contents):
 
 			sys.stderr.write(highlighted_text)
 			sys.stderr.write(highlighted_answer)
-			sys.stderr.write('\n\033[7m—————————————————————————\033[0m\n')
 	
 def find_required_words(answer):
 	for match in re.finditer(r'<u>(.*?)</u>', answer):
@@ -61,14 +71,12 @@ def find_required_words(answer):
 		for required_word in removed_tags.split():
 			if not STOPWORDS.match(required_word):
 				yield required_word
+				for inflected_word in get_inflected_forms_regex(required_word):
+					if not STOPWORDS.match(inflected_word):
+						yield inflected_word
 
 def required_words_as_regex(required_set):
-	for required_word in required_set:
-		inflections = sorted(list(set(get_inflected_forms_regex(required_word))))
-		print(required_word, inflections)
-
-	required_boundaries = map(lambda required_word: f'\b{required_word}\b', required_set)
-	return '|'.join(required_boundaries)
+	return r'(?i)\b(' + '|'.join(required_set) + r')'
 
 def split_into_text_and_answers(contents):
 	split = []
