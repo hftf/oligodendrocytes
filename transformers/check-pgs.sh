200000245038da7069e0ca0f3fcc157a9a410cd4
Ophir Lifshitz <hangfromthefloor@gmail.com>
2024-03-11 15:52:22 -0400

    Check PGs WIP (Oct 1, 2022)

---
+++ transformers/check-pgs.sh
@@ -0,0 +1,18 @@
+[^ ."][ˌˈ]
+low diacritic with descender and vice versa
+pinyin devoiced: b, d, g, j, zh. ... cn: not aspirated
+	p t k tɕ ʈʂ
+ɜɹ only stressed
+r -> ɹ
+ɹ\.
+chars not allowed in PG proper: " , : - g
+"ˈ[^. ]+"
+ɤ
+tags <...>
+n̠
+̯
+dental diacritic
+repeated tone letter
+tie bar
+
+# report count by flag
